b9f1848e148e246ec538569c92f57efae80a8125
stephenallison <steve.allison@clara.co.uk>
2025-07-06 10:16:45 +0100

    Setup tailwind

--- SignupApp/settings.py
+++ SignupApp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'signup.apps.SignupConfig',
+    'tailwind',
+    'theme'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
