88db4db5c5ad2c92777a942a83f567232ff06f93
stephenallison <steve.allison@clara.co.uk>
2025-07-27 22:20:53 +0100

    map / url

--- SignupApp/urls.py
+++ SignupApp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', include('signup.urls')),
+    path('', RedirectView.as_view(url='/signup/', permanent=False)),
 ]
