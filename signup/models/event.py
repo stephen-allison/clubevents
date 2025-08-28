18ffad5f9b8a4e03b14a2dd6af41d50aab267d1c
stephenallison <steve.allison@clara.co.uk>
2025-08-28 19:49:07 +0100

    add 'email sent' fields onto event model

--- signup/models/event.py
+++ signup/models/event.py
@@ -10,6 +10,8 @@ class Event(models.Model):
     location = models.CharField(max_length=512, blank=True, help_text="Event location")
     geo = models.CharField(max_length=100, blank=True, help_text="Coordinates (lat,lng) for mapping")
     organiser_email = models.EmailField(blank=True, help_text="Organiser email")
+    reminder_email_sent = models.BooleanField(default=False)
+    participants_email_sent = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name} ({self.date.isoformat()})'
